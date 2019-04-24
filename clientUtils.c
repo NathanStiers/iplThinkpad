af80f238c549f76a21667ea25d466a8d6cdb31bf
Stiers <nathan.stiers@gmail.oom>
2019-04-24 14:20:56 +0200

    feat: début client

--- clientUtils.c
+++ clientUtils.c
@@ -1,5 +1,34 @@
-#include "clientUtils.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-void initClient(){
-	printf("WIP");
+#include "clientUtils.h"
+#include "utils.h"
+
+int sockfd;
+
+int initSocketClient(char ServerIP[16], int Serverport){
+	int ret;
+	struct sockaddr_in addr;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&addr,0,sizeof(addr));
+	addr.sin_family= AF_INET;
+	addr.sin_port= htons(Serverport);
+	inet_aton(ServerIP,&addr.sin_addr);
+	connect(sockfd, (struct sockaddr*) &addr, sizeof(addr));
+	return sockfd;
+}
+
+void ecrireMessageAuServeur(){
+	write(sockfd,&msg,sizeof(msg));
+	checkNeg(ret,"write client error");
+}
+
+void lireMessageDuServeur(){
+	read(sockfd,&msg,sizeof(msg));
+	checkNeg(ret,"read client error");
 }
--- utils.h
+++ utils.h
@@ -1,4 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define MAX_LONGUEUR 256
+
 void checkNeg();
+
+typedef struct {
+	char MessageText[MAX_LONGUEUR];
+	int code;
+} structMessage;
