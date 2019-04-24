ba1e8f65832d8ee140905744f64e8d5847275e34
rdupont <robin.dupont@student.vinci.be>
2019-04-24 15:40:05 +0200

    FEAT: Caballero

--- serveur.c
+++ serveur.c
@@ -1,8 +1,9 @@
+#include <sys/select.h>
 #include "utils.h"
 #include "serveurUtils.h"
 
 int main(int argc, char* argv[]){
-	int sockfd, newsockfd;
+	int sockfd, newsockfd, ret;
 	int nbrConnexion = 0;
 	int connexions[MAX_UTILISATEURS];
 	structMessage msg;
@@ -10,7 +11,9 @@ int main(int argc, char* argv[]){
 	//Init du serv
 	sockfd = initServeur(atoi(argv[1]));
 	printf("Serveur lancée sur le port : %d\n", atoi(argv[1]));
-
+	
+	fd_set rfds;
+	FD_ZERO(&rfds);
 	while(1){
 		printf("attente...\n");
 		newsockfd = accept(sockfd, NULL, NULL);
@@ -18,12 +21,30 @@ int main(int argc, char* argv[]){
 			lireMessageClient(&msg, newsockfd);
 			if(msg.code == DEMANDE_CONNEXION){
 				connexions[nbrConnexion] = newsockfd;
+				FD_SET(connexions[nbrConnexion], &rfds);
 				nbrConnexion++;
 
 				msg.code = CONNEXION_REUSSIE;
 				ecrireMessageClient(&msg,newsockfd);
 				printf("Connexion de %d:\n", connexions[nbrConnexion-1]);
 				printf("Il y a : %d connexions.\n", nbrConnexion);
+			}
+		}
+		while(nbrConnexion>0){
+			ret = select(FD_SETSIZE, &rfds, NULL, NULL, NULL);
+			checkNeg(ret, "Erreur select");
+			for(int i=0;i<nbrConnexion;i++){
+				if(FD_ISSET(connexions[i], &rfds)){
+					lireMessageClient(&msg, connexions[i]);
+					switch(msg.code){
+						case AJOUT:
+						printf("ajout\n");
+						break;
+						case EXEC:
+						printf("exec\n");
+						break;
+					}
+				}
 			}
 		}
 	}
--- utils.h
+++ utils.h
@@ -11,6 +11,8 @@
 #define DEMANDE_CONNEXION 1
 #define CONNEXION_REUSSIE 2
 #define CONNEXION_ECHOUEE 3
+#define AJOUT -1
+#define EXEC -2
 
 void checkCond(bool cond, char* msg);
 
