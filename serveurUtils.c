91502c31767ce455f016cf9d7e721e17b56dad8a
Stiers <nathan.stiers@gmail.oom>
2019-04-30 12:26:00 +0200

    fix: conflict resolution

--- serveurUtils.c
+++ serveurUtils.c
@@ -2,19 +2,16 @@
 #include "utils.h"
 
 int shm_id;
+int sem_id;
 
 int initServeur(int port){
 	struct sockaddr_in addr;
-    // socket creation
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    // prepare sockaddr to bind
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
-    // listen on all server interfaces
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
     bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
-    // simultaneous client max -> 5
     listen(sockfd, MAX_UTILISATEURS);
     return sockfd;
 }
@@ -34,13 +31,13 @@ void ecrireMessageClient(structMessage *msg, int sockfd){
 //******************************************************************************
 
 void init_shm() {
-  shm_id = shmget(KEY, sizeof(int), IPC_CREAT | 0666);
+  shm_id = shmget(KEY_SHM, sizeof(int), IPC_CREAT | 0666);
   checkNeg(shm_id, "Error shmget");
-  
   for(int i=0;i<1000;i++){
     listeProgramme[i] = shmat(shm_id, NULL, 0);
   }
   checkCond(listeProgramme == (void*) -1, "Error shmat");
+
 }
 
 void shmdtCheck() {
@@ -51,5 +48,41 @@ void shmdtCheck() {
 void detruire_shm() {
   int r = shmctl(shm_id, IPC_RMID, NULL);
   checkNeg(r, "Error shmctl");
+}
+
+//******************************************************************************
+//SEMAPHORES
+//******************************************************************************
+
+void init_sem(int val) {
+
+  sem_id = semget(KEY_SEM, 1, IPC_CREAT | 0666);
+  checkNeg(sem_id, "Error semget");
+  union semun arg; 
+  arg.val = val;
+  int rv = semctl(sem_id, 0, SETVAL, arg);
+  checkNeg(rv, "Error semctl");
+}
+
+void add_sem(int val) {
+  struct sembuf sem; 
+  sem.sem_num = 0; 
+  sem.sem_op = val; 
+  sem.sem_flg = 0;
+  int rc = semop(sem_id, &sem, 1);
+  checkNeg(rc, "Error semop");
+}
+
+void down() {
+  add_sem(-1);
+}
+
+void up() {
+  add_sem(1);
+}
+
+void del_sem() {
+  int rv = semctl(sem_id, 0, IPC_RMID);
+  checkNeg(rv, "Error semctl");
 }
 
--- serveurUtils.h
+++ serveurUtils.h
@@ -9,12 +9,14 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/ipc.h>
+#include <sys/sem.h>
 #include <sys/shm.h>
 #include <string.h>
 #include "utils.h"
 
 #define MAX_UTILISATEURS 50
-#define KEY 42
+#define KEY_SHM 42
+#define KEY_SEM 4242
 #define TAILLEPHYSIQUE 1000
 
 typedef struct{
@@ -25,7 +27,15 @@ typedef struct{
 	long dureeExecTotal;
 } Programme;
 
+union semun {
+   int              val;    /* Value for SETVAL */
+   struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+   unsigned short  *array;  /* Array for GETALL, SETALL */
+   struct seminfo  *__buf;  /* Buffer for IPC_INFO (Linux-specific) */
+};
+
 Programme* listeProgramme[TAILLEPHYSIQUE];
+
 int tailleLogique = 0;
 
 int initServeur(int port);
@@ -39,5 +49,15 @@ void init_shm();
 void shmdtCheck();
 
 void detruire_shm();
+
+void init_sem(int val);
+
+void add_sem(int val);
+
+void down();
+
+void up();
+
+void del_sem();
 
 #endif
